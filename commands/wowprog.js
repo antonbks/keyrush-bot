6f744b80fe887f9e627de8347cca82cf1df36085
Buksa Anton <anton.buksa@queensu.ca>
2017-06-19 15:44:23 +0200

    fix

--- commands/wowprog.js
+++ commands/wowprog.js
@@ -94,7 +94,7 @@ module.exports = function (args, user, userID, channelID, bot) {
                 keyscore = temp2
                 otherData2 = "SimDPS: Unavailable for Healer/Tank"
             }
-
+            scoreIndex = keyscore.match(/\d/)
             rankTable[capitalize(argsArray[0])] = parseInt(keyscore.substring(keyscore.indexOf(scoreIndex)))
             fs.writeFile("rankings.json", JSON.stringify(rankTable), "utf8");
 
